a48180080cc1634bdbb6983c4d6f8839561dfcbf
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-05-14 10:40:59 +0600

    added category section

--- src/Routers/router.jsx
+++ src/Routers/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayOut from "../MainLayOut/MainLayOut";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home/Home";
+ 
 
 export const router = createBrowserRouter([
     {
@@ -9,7 +10,7 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Home></Home>
+                element:<Home></Home>
             }
         ]
     },
