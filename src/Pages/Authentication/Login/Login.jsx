52f01ad1513f2d701de2e5c1a22238d9dfac9243
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-05-22 16:49:17 +0600

    added sweet alert

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,42 +1,63 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LoadCanvasTemplate, loadCaptchaEnginge, validateCaptcha } from "react-simple-captcha";
 import { AuthContext } from "../../../Provider/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 
 const Login = () => {
-     const captchaRef = useRef(null)
-     const [disable,setDisable]=useState(true)
-     const {signIn}=useContext(AuthContext)
+     const navigate=useNavigate()
+    const [disable, setDisable] = useState(true)
+    const { signIn } = useContext(AuthContext)
     const handleSubmit = (e) => {
         e.preventDefault()
         const form = e.target
         const email = form.email.value
         const password = form.password.value
         console.log(email, password)
-        signIn(email,password)
-        .then(data=>{
-            const user = data.user
-            console.log(user)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    }
-    useEffect(()=>{
-            loadCaptchaEnginge(6); 
-    },[])
+        signIn(email, password)
+            .then(data => {
+                const user = data.user
+                if (user) {
+                    Swal.fire({
+                        title: "user successfully created",
+                        showClass: {
+                            popup: `
+                                    animate__animated
+                                    animate__fadeInUp
+                                    animate__faster
+                                    `
+                        },
+                        hideClass: {
+                            popup: `
+                                    animate__animated
+                                    animate__fadeOutDown
+                                    animate__faster
+                                                    `
+                        }
+                    });
+                    navigate("/")
+                }
 
-    const handleCaptcha=()=>{
-        const userCaptchaValue = captchaRef.current.value 
-         if(validateCaptcha(userCaptchaValue)){
-            setDisable(false)
-         }else{
-            setDisable(true)
-         }
+            })
+            .catch(err => {
+
+            })
     }
-    
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
+    const handleCaptcha = (e) => {
+        const userCaptchaValue = e.target.value
+        if (validateCaptcha(userCaptchaValue)) {
+            setDisable(false)
+        } else {
+            setDisable(true)
+        }
+    }
+
     return (
         <div className="min-h-screen hero bg-base-200">
             <div className="flex-col hero-content lg:flex-row-reverse">
@@ -56,17 +77,17 @@ const Login = () => {
                             <input type="password" className="input" placeholder="Password" name="password" />
                             {/* for captcha-------start------ */}
                             <div className="h-20 my-3 border border-red-500">
-                                      <LoadCanvasTemplate />
+                                <LoadCanvasTemplate />
                             </div>
-                             <div className="">
-                                   <input type="text" className="mb-3 input" placeholder="type your captcha " name="captcha" ref={captchaRef}/>
-                                  <button className="w-full mb-3 btn link link-hover btn-xs btn-outline" onClick={handleCaptcha}>Validate</button>
-                             </div>
-                                {/* for captcha-----end-------- */}
+                            <div className="">
+                                <input type="text" className="mb-3 input" placeholder="type your captcha " name="captcha" onBlur={handleCaptcha} required />
+                                <small className="text-orange-500">Type the captcha and click outside the box to verify.</small>
+                            </div>
+                            {/* for captcha-----end-------- */}
                             <div>
-                               <Link to={"/signUp"}>
-                                   <button className="text-red-500 link link-hover">Have an account?Go to sign Up</button>
-                               </Link>
+                                <Link to={"/signUp"}>
+                                    <button className="text-red-500 link link-hover">Do not an account?Go to sign Up</button>
+                                </Link>
                             </div>
                             <button className="mt-4 btn btn-neutral" disabled={disable}>Login</button>
                         </fieldset>
--- src/Pages/Authentication/SignUp/SignUp.jsx
+++ src/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,6 +1,53 @@
 
+import { useContext } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+    const { createUser } = useContext(AuthContext)
+    const navigate=useNavigate()
+    const {
+        register,
+        handleSubmit,
+
+        formState: { errors },
+
+    } = useForm()
+
+    const onSubmit = (data) => {
+        createUser(data.email, data.password)
+            .then(result => {
+                if (result.user) {
+                    Swal.fire({
+                        title: "user successfully created",
+                        showClass: {
+                            popup: `
+                                        animate__animated
+                                        animate__fadeInUp
+                                        animate__faster
+                                        `
+                        },
+                        hideClass: {
+                            popup: `
+                                    animate__animated
+                                    animate__fadeOutDown
+                                    animate__faster
+                                    `
+                        }
+                    });
+                    navigate("/login")
+                }
+                
+            })
+            .catch(err => {
+
+            })
+
+    }
+
+
     return (
         <div className="min-h-screen hero bg-base-200">
             <div className="flex-col hero-content lg:flex-row-reverse">
@@ -12,20 +59,39 @@ const SignUp = () => {
                     </p>
                 </div>
                 <div className="w-full max-w-sm shadow-2xl card bg-base-100 shrink-0">
-                    <div className="card-body">
+                    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                         <fieldset className="fieldset">
                             <label className="label">Name</label>
-                            <input type="text" className="input" placeholder="Name" name="name"/>
-                            
+                            <input type="text" className="input" placeholder="Name"   {...register("name", { required: "name is required" })} />
+                            {errors.name && <span className="text-red-500">{errors.name.message}</span>}
+
                             <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" name="email"/>
-                            
+                            <input type="email" className="input" placeholder="Email"  {...register("email", { required: "email is required" })} />
+                            {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+
                             <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" name="password"/>
+                            <input type="password" className="input" placeholder="Password"  {...register("password",
+                                {
+                                    required: "password is required",
+                                    minLength: {
+                                        value: 6,
+                                        message: "6 character is required"
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                                        message: "Password must contain at least one uppercase, one lowercase, one number, and one special character (!@#$&*)",
+                                    }
+                                },
+
+                            )} />
+                            {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+
+
                             <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="mt-4 btn btn-neutral">signUp</button>
+                            <input type="submit" className="mt-4 btn btn-neutral" value={'signUp'} />
+
                         </fieldset>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
