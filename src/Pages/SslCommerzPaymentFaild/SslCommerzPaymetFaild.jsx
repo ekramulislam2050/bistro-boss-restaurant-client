9d4854acd92eb598f80f951f7a55df6593570e98
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-11-02 15:11:03 +0600

    not complete

--- src/Pages/SslCommerzPaymentFaild/SslCommerzPaymetFaild.jsx
+++ src/Pages/SslCommerzPaymentFaild/SslCommerzPaymetFaild.jsx
@@ -3,7 +3,7 @@
 const SslCommerzPaymetFaild = () => {
     return (
         <div>
-            <h1 className="text-5xl text-center"> payment hoi nai-----------</h1>
+            <h1 className="text-5xl text-center text-red-500"> payment hoi nai-----------</h1>
         </div>
     );
 };
